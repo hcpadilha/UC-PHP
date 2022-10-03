c1fc3ee1c4dc42728ee4046d598fbc7b28dd2e91
Henrique Padilha <102041901+hcpadilha@users.noreply.github.com>
2022-10-03 20:19:28 -0300

    AULA_01

---
+++ code.js
@@ -0,0 +1,16 @@
+let primeiroNum = 3;
+let segundoNum = 2;
+
+let soma =  (primeiroNum + segundoNum);
+console.log(`A soma é :  ${soma}`);
+console.log(`A multiplicação é:  ${soma * primeiroNum} `);
+
+
+
+
+let terceiroNum = 3;
+let quartoNum = 4;
+let quintoNum = 8;
+
+let media = (terceiroNum + quartoNum + quintoNum)/3;
+console.log(`A média é ${media.toFixed(2)}`);
